1795559c18a4bc1f696be3de89905e9f9a501dc1
Mridul54 <dasmridul364@gmail.com>
2025-04-18 18:07:51 +0600

    recap conditionals

---
+++ python/conditional.py
@@ -0,0 +1,19 @@
+# marks= int(input("Enter your marks:"))
+
+# if(marks >= 90):
+#     grade = "A++"
+# elif(marks >=80 and marks<90):
+#     grade = "A+"
+# elif(marks >=70 and marks<80):
+#     grade= "A"
+# else:
+#     grade= "fail"
+# print("Grade of the student:", grade)        
+
+
+num= int(input("enter a number:"))
+
+if(num % 6 == 0):
+    print("multiple is 6")
+else:
+    print("can't multiple")
---
+++ python/variables.py
@@ -0,0 +1,16 @@
+# num1= int(input("the first num:"))
+# num2= int(input("the second num:"))
+# sum= num1 + num2
+# print("total sum is", sum)
+
+#slicing(also have positive and negative index)
+# str= "mriduldas"
+# print(str[3:len(str)])
+
+#string functions
+str = " t his is my computer and i can use that smoothly"
+#print(str.endswith("tly"))
+# str= str.capitalize()
+#print(str.replace("u", "g"))
+print(str.find("my"))
+print(str.count("t"))
